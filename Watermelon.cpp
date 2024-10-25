1464dda724b27f9b1d9ed1e6d41d85852e818412
Tanvir Alam <104571092+Tanviralam007@users.noreply.github.com>
2024-10-25 11:38:47 +0600

    Add files via upload

---
+++ Watermelon.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+int main() {
+	int w;
+	std::cin >> w;
+	if(w % 2 == 0 && w > 2) std::cout << "YES" << std::endl;
+	else std::cout << "NO" << std::endl;
+
+    return 0;
+}
+
