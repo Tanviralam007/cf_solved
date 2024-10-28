a52c70569eed2a0f627ce996230058b1b33175fa
Tanvir Alam <104571092+Tanviralam007@users.noreply.github.com>
2024-10-28 17:17:45 +0600

    Word.cpp

--- Word.cpp
+++ Word.cpp
@@ -7,18 +7,15 @@ int main()
 
 	int u = 0, l = 0;
 	for(char c : s){
-		if(std::isupper(c))
-			u++;
-		else
-			l++;
+		if(std::isupper(c)) u++;
+		else l++;
 	}
 
 	if(u > l)
 		std::transform(s.cbegin(), s.cend(), s.begin(), ::toupper);
 	else if(l > u)
 		std::transform(s.cbegin(), s.cend(), s.begin(), ::tolower);
-	else
-		std::transform(s.cbegin(), s.cend(), s.begin(), ::tolower);
+	else std::transform(s.cbegin(), s.cend(), s.begin(), ::tolower);
 
 	std::cout << s << std::endl;
 
